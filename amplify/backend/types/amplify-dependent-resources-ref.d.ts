04c6b6e813b0154d503f9ed8a2de298559732ff8
yanlin96 <yanlin96.gong@gmail.com>
2022-08-10 14:34:38 +1000

    dev: push the lambda function with env API_URL to cloud

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,10 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "multienv": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
